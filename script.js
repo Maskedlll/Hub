838be999f3ce8690cf5386f344acefb61617ad74
Masked <mkt_vio@outlook.com>
2025-01-15 20:35:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// Disable Zoom
+document.addEventListener("touchmove", function (event) {
+  if (event.scale !== 1) {
+    event.preventDefault();
+  }
+}, { passive: false });
 
+// Toggle Menu
+function toggleMenu(icon) {
+  icon.classList.toggle("change");
+  const sideMenu = document.getElementById("sideMenu");
+  sideMenu.style.left = sideMenu.style.left === "0px" ? "-250px" : "0px";
+}
